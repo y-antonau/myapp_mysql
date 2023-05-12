9b222282f3e5cba1f099dae66ef249e915141363
root <root@ANTBG>
2023-05-12 16:40:02 +0300

    first step

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+  get 'hello', to: 'pages#contact'
+  get 'pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "pages#index"
 end
